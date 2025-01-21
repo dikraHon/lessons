907a918c64ed633e30bd1d119a924775b1690d6e
Dikra <Dikramfvdf>
2025-01-21 14:26:34 +0300

    add fun reversalWord

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        reversalOfWord();
+    }
+    public static void reversalOfWord(){
+        System.out.println("Enter your message: ");
+        String str = new Scanner(System.in).nextLine();
+        StringBuilder stringReverse = new StringBuilder();
+        str += " ";
+        int i = 0;
+        String stroka = "";
+        while (i < str.length() ){
+            if (str.charAt(i) == ' '){
+                stringReverse.insert(0, stroka + " ");
+                stroka = "";
+                i++;
+                continue;
+            }
+            stroka = stroka + str.charAt(i);
+            i++;
+        }
+        System.out.println(stringReverse);
     }
 }
